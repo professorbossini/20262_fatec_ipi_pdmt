5a5ef8a5a6e8e21918cf5c16da1b8a89cf303875
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-21 17:42:11 -0300

    learn(ap04-react): encerra a apostila

---
+++ ap04-react-props/src/Cartao.jsx
@@ -0,0 +1,16 @@
+// rafce
+const Cartao = (props) => {
+  return (
+    <div className="card">
+      <div className="card-header text-muted">
+        {props.cabecalho}
+      </div>
+      <div className="card-body">
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+
+export default Cartao
